947d9332f96ad78d6572066d4d91e396d11be0ef
sakiyamamamama <l220429@mail.ryukoku.ac.jp>
2023-10-28 00:06:01 +0900

    DB改善

--- accounts/models.py
+++ accounts/models.py
@@ -1,21 +1,18 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.conf import settings
 
-#ユーザーアカウント
 class CustomUser(AbstractUser):
     groups = models.ManyToManyField('auth.Group', related_name='custom_user_groups')
     user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_permissions')
 
-#本
 class Book(models.Model):
     title = models.CharField(max_length=100)
     is_borrowed = models.BooleanField(default=False)
+    tags = models.ManyToManyField('Tag', related_name='books', blank=True)
 
-#タグ
 class Tag(models.Model):
     name = models.CharField(max_length=50)
-    books = models.ManyToManyField(Book)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
